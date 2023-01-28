23428631b51a37362fe7c93ab4d6206b11a1fa26
NikitaKakurin <neyasut87@rambler.ru>
2023-01-28 15:10:30 +0300

    feat: add simple animation task 2

--- task2/index.js
+++ task2/index.js
@@ -1,1 +1,18 @@
-alert('!!!!!!!!!!!работает!!!!!!!!!')
+circle.onclick = function(){
+  this.classList.add('to-right')
+}
+
+rectangle.onclick = function(){
+  console.log('rect-anim');
+  this.classList.add('rect-anim')
+}
+
+square.onclick = function(){
+  console.log('square-anim');
+  this.classList.add('square-anim')
+}
+
+triangle.onclick = function(){
+  console.log('triangle-anim');
+  this.classList.add('triangle-anim')
+}
