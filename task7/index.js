2516a65d83e6b42121b5e03e54706bec9521925e
NikitaKakurin <neyasut87@rambler.ru>
2023-01-29 15:05:41 +0300

    feat: finish task 7

--- task7/index.js
+++ task7/index.js
@@ -1,1 +1,15 @@
-alert('!!!!!!!!!!!работает!!!!!!!!!')
+const container = document.querySelector('.container');
+
+const getRandomNumber=()=> Math.floor(Math.random()*90 + 10);
+const getRandomColor=()=> `#${Math.floor(Math.random()*16777215).toString(16)}`;
+
+const count = getRandomNumber();
+const squares = []
+
+for (let index = 0; index < count; index++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.style.backgroundColor = getRandomColor();
+  squares.push(square)
+}
+container.append(...squares)
